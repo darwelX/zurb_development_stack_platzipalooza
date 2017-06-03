496e15ba84ea9472a2c246182009d918f96824f9
Darwel Quintero <darwel34@gmail.com>
2017-06-03 13:11:15 -0430

    se agrego la logica para animar los elementos

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -8,3 +8,54 @@ const $offCanvas = $('#offCanvas');
 $offCanvas.find('li').click(function (ev){
     $offCanvas.foundation('close');
 });
+
+function animateInViernes(){
+    var $animation1 = $("#guns-uno").data("animation");
+    var $animation2 = $("#guns-dos").data("animation");
+    var $animation3 = $("#guns-tres").data("animation");
+    var $animation4 = $("#guns-cuatro").data("animation");
+    Foundation.Motion.animateIn($("#guns-uno"), $animation1);
+    Foundation.Motion.animateIn($("#guns-dos"), $animation2);
+    Foundation.Motion.animateIn($("#guns-tres"), $animation3);
+    Foundation.Motion.animateIn($("#guns-cuatro"), $animation4);
+}
+
+function animateInSabado(){
+    var $animation1 = $("#beatles-uno").data("animation");
+    var $animation2 = $("#beatles-dos").data("animation");
+    var $animation3 = $("#beatles-tres").data("animation");
+    var $animation4 = $("#beatles-cuatro").data("animation");
+    Foundation.Motion.animateIn($("#beatles-uno"), $animation1);
+    Foundation.Motion.animateIn($("#beatles-dos"), $animation2);
+    Foundation.Motion.animateIn($("#beatles-tres"), $animation3);
+    Foundation.Motion.animateIn($("#beatles-cuatro"), $animation4);
+}
+
+function animateInDomingo(){
+    var $animation1 = $("#queen-uno").data("animation");
+    var $animation2 = $("#queen-dos").data("animation");
+    var $animation3 = $("#queen-tres").data("animation");
+    var $animation4 = $("#queen-cuatro").data("animation");
+    Foundation.Motion.animateIn($("#queen-uno"), $animation1);
+    Foundation.Motion.animateIn($("#queen-dos"), $animation2);
+    Foundation.Motion.animateIn($("#queen-tres"), $animation3);
+    Foundation.Motion.animateIn($("#queen-cuatro"), $animation4);
+}
+
+const $tabViernes = $('#viernes, #viernes-sticky, #viernes-canvas');
+$tabViernes.click(function(eve){
+    animateInViernes();
+});
+
+const $tabSabado = $('#sabado, #sabado-sticky, #sabado-canvas');
+$tabSabado.click(function(eve){
+    animateInSabado();
+});
+
+const $tabDomingo = $('#domingo, #domingo-sticky, #domingo-canvas');
+$tabDomingo.click(function(eve){
+    animateInDomingo();
+});
+
+animateInViernes();
+
